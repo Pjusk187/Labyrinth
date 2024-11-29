7468a55284259c65c5981ea9c61e038a9d9d7f71
Pjusk187 <maggeren@hotmail.no>
2024-11-29 17:08:30 +0100

    Add files via upload

---
+++ labyrint-main/game.mjs
@@ -0,0 +1,35 @@
+import Labyrinth from "./labyrint.mjs"
+import SplashScreen from "./splashScreen.mjs";
+import ANSI from "./utils/ANSI.mjs";
+
+const REFRESH_RATE = 250;
+
+console.log(ANSI.RESET, ANSI.CLEAR_SCREEN, ANSI.HIDE_CURSOR);
+
+let intervalID = null;
+let isBlocked = false;
+let state = null;
+
+function startGame() {
+    //All levels available to the game. 
+    
+    const splash = new SplashScreen(); 
+
+    splash.animate(() => {
+    state = new Labyrinth(() => clearInterval(intervalID));
+    intervalID = setInterval(update, REFRESH_RATE);
+    });
+}
+
+function update() {
+
+    if (isBlocked) { return; }
+    isBlocked = true;
+    //#region core game loop
+    state.update();
+    state.draw();
+    //#endregion
+    isBlocked = false;
+}
+
+startGame();
